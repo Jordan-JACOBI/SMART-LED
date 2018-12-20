c459f382049c09787b8984fb556695a347cbc465
Jordan-JACOBI <32301879+Jordan-JACOBI@users.noreply.github.com>
2018-12-20 15:38:22 +0100

    added sensor_topics
    attribute a unique topic to a each sensor reusable in send methods

--- SMART_LED/slave.py
+++ SMART_LED/slave.py
@@ -11,7 +11,11 @@ class Slave :
 		# ATTRIBUTE SLAVES
 		self.sensors = [0,0,0,0,0,0,0,0,0,0]
 		self.define_sensors()
-
+		
+		# ATTRIBUTE SENSOR TOPICS
+		self.sensor_topics = ["","","","","","","","","",""]
+		self.define_sensor_topics()
+		
 		# CREATE MQTT CLIENT
 		self.client = mqtt.MQTTClient()
 
@@ -34,22 +38,33 @@ class Slave :
 	def define_sensors(self) :
 		self.sensors.insert(SensorList.TEMPERATURE.value, "Temp sensor")
 		self.sensors.insert(SensorList.HUMIDITY.value, "Humidity sensor")
-		self.sensors.insert(SensorList.IR.value, "IR sensor")
+		self.sensors.insert(SensorList.PIR.value, "PIR sensor")
 		self.sensors.insert(SensorList.BRIGHTNESS.value, "Bright sensor")
-
-
+	
+	# ATTRIBUTION OF ALL SENSOR TOPICS
+	def define_sensor_topics(self) :
+		self.attribute_topic(SensorList.TEMPERATURE.value, topics.TEMPERATURE)
+		self.attribute_topic(SensorList.HUMIDITY.value, topics.HUMIDITY)
+		self.attribute_topic(SensorList.PIR.value, topics.PRESENCE)
+		self.attribute_topic(SensorList.BRIGHTNESS.value, topics.BRIGHTNESS)
+		
+	# ATTRIBUTE A TOPIC TO A SENSOR
+	def attribute_topic(self, sensor_id, topic) :
+		full_topic = topics.format(topic, self.slave_address)
+		self.sensor_topics.insert(sensor_id, full_topic)
+	
 	# GetTemperature from sensor
 	def getTemperature(self) :
 		return 19.4
 
 	def sendTemperature(self) :
-		self.client.publish('ECAM/jordan', self.getTemperature(), 1, False)
+		self.client.publish(self.sensor_topics[SensorList.TEMPERATURE.value], self.getTemperature(), 1, False)
 	
 	# GET SENSOR VALUE
 	def getValue(self, index) :
 		print(self.sensors[index.value])
-		#return self.sensors[index].getValue()
+		return self.sensors[index.value].getValue()
 
 	# SEND SENSOR VALUE OVER MQTT
 	def sendValue(self, index) :
-		pass
+		self.client.publish(self.sensor_topics[index.value], self.getValue(index), 1, False)
